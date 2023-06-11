ab0cfd553305d56bb17fedcfce7243579f1df463
Ress1206-bit <adam.ress@icloud.com>
2023-06-10 21:59:08 -0400

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Adam Ress on 6/10/23.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
