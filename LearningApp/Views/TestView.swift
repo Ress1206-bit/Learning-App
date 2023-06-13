bbee49e19c3a8156b2142e2fe98c51fb9811a06a
Ress1206-bit <adam.ress@icloud.com>
2023-06-12 23:40:59 -0400

    - submit button in test view

--- LearningApp/Views/TestView.swift
+++ LearningApp/Views/TestView.swift
@@ -10,18 +10,80 @@ import SwiftUI
 struct TestView: View {
     
     @EnvironmentObject var model: ContentModel
-    
-    
+    @State var selectedAnswerIndex: Int?
+    @State var numCorrect = 0
+    @State var submitted = false
     
     var body: some View {
         
-        
-        
         if model.currentQuestion != nil {
-            VStack{
+            VStack(alignment: .leading){
                 Text("Question \(model.currentQuestionIndex + 1) of \(model.currentModule?.test.questions.count ?? 0)")
+                    .padding(.leading, 20)
                 
                 CodeTextView()
+                    .padding(.horizontal, 20)
+                
+                ScrollView{
+                    VStack{
+                        ForEach(0..<model.currentQuestion!.answers.count, id: \.self){ index in
+                            
+                            Button {
+                                selectedAnswerIndex = index
+                                
+                            } label: {
+                                ZStack{
+                                    if !submitted {
+                                        RectangleCard(color: index == selectedAnswerIndex ? .gray : .white)
+                                            .frame(height: 48)
+                                    }
+                                    else {
+                                        if index == selectedAnswerIndex && index == model.currentQuestion!.correctIndex{
+                                            RectangleCard(color: .green)
+                                                .frame(height: 48)
+                                        }
+                                        else if index == selectedAnswerIndex && index != model.currentQuestion!.correctIndex {
+                                            RectangleCard(color: .red)
+                                                .frame(height: 48)
+                                        }
+                                        else if index == model.currentQuestion?.correctIndex {
+                                            RectangleCard(color: .green)
+                                                .frame(height: 48)
+
+                                        }
+                                        else {
+                                            RectangleCard(color: .white)
+                                                .frame(height: 48)
+                                        }
+                                    }
+                                    Text(model.currentQuestion!.answers[index])
+                                }
+                            }
+                            .disabled(submitted)
+                            .buttonStyle(CustomButtonStyle())
+                        }
+                    }
+                    .accentColor(.black)
+                    .padding()
+                }
+                
+                Button {
+                    submitted = true
+                    if selectedAnswerIndex == model.currentQuestion!.correctIndex {
+                        numCorrect += 1
+                    }
+                } label: {
+                    ZStack{
+                        RectangleCard(color: .green)
+                            .frame(height: 48)
+                        Text("Submit")
+                            .bold()
+                            .foregroundColor(.white)
+                    }
+                    .padding()
+                }
+                .disabled(selectedAnswerIndex == nil)
+
             }
             .navigationTitle("\(model.currentModule?.category ?? "") Test")
             
@@ -36,5 +98,13 @@ struct TestView_Previews: PreviewProvider {
     static var previews: some View {
         TestView()
             .environmentObject(ContentModel())
+    }
+}
+
+
+struct CustomButtonStyle: ButtonStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .opacity(configuration.isPressed ? 1.0 : 0.8)
     }
 }
